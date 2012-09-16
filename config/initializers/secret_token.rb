9e968bf7204a3f11038e783bb2a458f097038c36
HINHIN <hin.tm2d@gmail.com>
2012-09-17 00:12:00 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RecordLib::Application.config.secret_token = '151b42a4b753afa1e41671da8b8e1c10b4d9251c0ae3a6ee2bb6a001f7f549812f5f30c86be71d0248bff504db9a7234f981a3be9cf8730d3792141c7fdc20d5'
